6a7dd63089bb2a2b565700245ad4c59e50e9dfe5
Cyriacph <116813319+Cyriacph@users.noreply.github.com>
2024-02-20 21:47:39 +0100

    class Direction, début

---
+++ src/ch/epfl/chacun/Direction.java
@@ -0,0 +1,37 @@
+package ch.epfl.chacun;
+
+import java.util.List;
+
+/**
+ * Une direction des quatre points cardinaux
+ *
+ * @author Cyriac Philippe (360553)
+ */
+public enum Direction {
+    N,
+    E,
+    S,
+    W;
+
+    private final static List<Direction> ALL = List.of(Direction.values());
+    private final static int COUNT = ALL.size();
+
+    /**
+     * Direction après une rotation
+     *
+     * @return TBD
+     */
+    public Direction rotates( ) {
+        //int rotatedDirectionIndex = ordinal() + rotation.ordinal();
+        return null;
+    }
+
+    /**
+     * Inverse de la direction
+     * @return ALL.get((ordinal()+COUNT/2)%COUNT)
+     *          direction inverse de this
+     */
+    public Direction opposite() {
+        return ALL.get((ordinal()+COUNT/2)%COUNT);
+    }
+}
