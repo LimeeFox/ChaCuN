3e7ff61cb4484d06f3acbad16b0973c661b97c1c
Cyriacph <116813319+Cyriacph@users.noreply.github.com>
2024-02-21 22:17:56 +0100

    record Occupant, WIP

---
+++ src/ch/epfl/chacun/Occupant.java
@@ -0,0 +1,45 @@
+package ch.epfl.chacun;
+
+import java.util.Objects;
+
+/**
+ * Occupant d'une tuile identifiée
+ *
+ * @param kind
+ *          type d'occupant : pion, hutte
+ * @param zoneId
+ *          identifiant de la zone
+ */
+public record Occupant(Kind kind, int zoneId) {
+
+    // Types d'occupants
+    public enum Kind {
+        PAWN,
+        HUT
+    }
+
+    // Constructeur compact
+    public Occupant {
+        Objects.requireNonNull(kind);
+        Preconditions.checkArgument(zoneId >= 0);
+    }
+
+    /**
+     * Conte les occupants d'une même sorte que possède un joueur
+     *
+     * @param kind
+     *          type d'occupants
+     * @return occupantsOfKind
+     *          nombre total d'occupants du même type que possède
+     *          un joueur
+     */
+    public static int occupantsCount(Kind kind) {
+        int occupantsOfKind = 0;
+        if (kind.equals(Kind.PAWN)) {
+            occupantsOfKind = 5;
+        } else {
+            occupantsOfKind = 3;
+        }
+        return occupantsOfKind;
+    }
+}
