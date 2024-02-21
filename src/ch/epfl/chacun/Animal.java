15d985fcf29aaffed3d348f1ddb0336bad060bfc
Cyriacph <116813319+Cyriacph@users.noreply.github.com>
2024-02-21 22:34:42 +0100

    record Animal, WIP

--- src/ch/epfl/chacun/Animal.java
+++ src/ch/epfl/chacun/Animal.java
@@ -1,4 +1,32 @@
 package ch.epfl.chacun;
 
-public record Animal() {
+/**
+ * Animal avec un identifié sur le plateau
+ *
+ * @author Cyriac Philippe (360553)
+ *
+ * @param id
+ *          identifiant de l'animal
+ * @param kind
+ *          type de l'animal : mammouth, auroch,
+ *          cerf, smilodon
+ */
+public record Animal(int id, Kind kind) {
+    enum Kind {
+        MAMMOTH,
+        AUROCHS,
+        DEER,
+        TIGER
+    }
+
+    /**
+     * Identifiant de la case de l'animal
+     *
+     * @return
+     */
+    public int tileId() {
+        //necessite Zone pour la completion
+        int animalTileId = id;
+
+    }
 }
