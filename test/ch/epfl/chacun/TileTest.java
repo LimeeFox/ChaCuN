362a6bda84f778635ecd7a0da45c18bedf4243ea
Cyriacph <116813319+Cyriacph@users.noreply.github.com>
2024-02-28 20:27:30 +0100

    WIP: feat/test2/TileTest

--- test/ch/epfl/chacun/TileTest.java
+++ test/ch/epfl/chacun/TileTest.java
@@ -1,5 +1,16 @@
 package ch.epfl.chacun;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
 public class TileTest {
+
+    @Test
+    void tileSideZonesWorks() {
+        Set<Zone> expectedSideZonesMeadow;
+        Set<Zone> expectedSideZonesRiver;
+        Set<Zone> expectedSideZonesForest;
+    }
 
 }
