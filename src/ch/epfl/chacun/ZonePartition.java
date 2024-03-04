c47ce2a31d3d8a2cb743cb12cbf4da595947d3e6
Cyriacph <116813319+Cyriacph@users.noreply.github.com>
2024-03-04 18:29:20 +0100

    WIP: feat/ZonePartition.Builder

--- src/ch/epfl/chacun/ZonePartition.java
+++ src/ch/epfl/chacun/ZonePartition.java
@@ -1,5 +1,7 @@
 package ch.epfl.chacun;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -13,6 +15,79 @@ import java.util.Set;
  */
 
 public record ZonePartition<Z extends Zone>(Set<Area<Z>> areas) {
+
+    public class Builder {
+        private Set<Area<Z>> areas;
+
+        /**
+         * Ajout d'une nouvelle aire inoccupée, constitué d'une zone donnée et d'un nombre de connections ouvertes
+         * donné, à la partition en cours de construction
+         *
+         * @param zone
+         *          zone constituant l'aire inoccupée
+         * @param openConnections
+         *          nombre de connections ouvertes de l'aire inoccupée
+         */
+        public void addSingleton(Z zone, int openConnections) {
+            areas.add(new Area<>(Set.of(zone), List.of(), openConnections));
+        }
+
+        /**
+         * Ajout d'un occupant d'une couleur donnée à l'aire contenant une zone donnée
+         *
+         * @param zone
+         *          zone à laquelle un occupant sera ajouter
+         * @param color
+         *          couleur de l'occupant à ajouter
+         *
+         * @throws IllegalArgumentException
+         *          si aucune aire de la partition en construction ne contient la zone donnée
+         */
+        public void addInitialOccupant(Z zone, PlayerColor color) {
+            areaContaining(zone).withInitialOccupant(color);
+        }
+
+        /**
+         * Suppression d'un occupant d'une couleur donnée de l'aire contenant une zone donnée
+         *
+         * @param zone
+         *          zone à laquelle l'occupant sera supprimé
+         * @param color
+         *          couleur de l'occupant qui sera supprimé
+         *
+         * @throws IllegalArgumentException
+         *          si aucune aire de la partition en construction ne contient la zone donnée
+         */
+        public void removeOccupant(Z zone, PlayerColor color) {
+            areaContaining(zone).withoutOccupant(color);
+        }
+
+        /**
+         * Suppression de tous les occupants d'une aire donnée
+         *
+         * @param area
+         *          aire dont la totalité des occupants seront supprimés
+         */
+        public void removeAllOccupant(Area<Z> area) {
+            area.withoutOccupants();
+        }
+
+        /**
+         * Connecte les aires contenant les zones zone1 et zone2 pour en faire une plus grande aire
+         *
+         * @param zone1
+         *          zone contenue dans la première aire
+         * @param zone2
+         *          zone contenue dans la deuxième aire
+         *
+         * @throws IllegalArgumentException
+         *          si une des deux aires n'est pas contenue dans une aire
+         */
+        public void union(Z zone1, Z zone2) {
+            // TODO: 04/03/2024 Finish connect to method 
+            areaContaining(zone1).connectTo(areaContaining(zone2));
+        }
+    }
 
     public ZonePartition {
         areas = Set.copyOf(areas);
