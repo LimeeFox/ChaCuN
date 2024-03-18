c5e995e6dac06b730f0066610f6b334e9d51e1c0
Cyriacph <116813319+Cyriacph@users.noreply.github.com>
2024-03-18 17:33:54 +0100

    WIP: feat/Board

---
+++ src/ch/epfl/chacun/Board.java
@@ -0,0 +1,29 @@
+package ch.epfl.chacun;
+
+/**
+ * Le tableau de jeu
+ *
+ * @author Cyriac Philippe (360553)
+ * @author Vladislav Yarkovoy (362242)
+ */
+
+public class Board {
+    //Are they really all "final"?
+    private final PlacedTile[] placedTiles = new PlacedTile[625];
+    //placedTileIndices may not have to be so big, considering there's only 96 total possible tiles
+    private final int[] placedTileIndices = new int[625];
+    private final ZonePartitions boardPartitions = new ZonePartitions(new ZonePartition<Zone.Forest>(),
+            new ZonePartition<Zone.Meadow>(),
+            new ZonePartition<Zone.River>(),
+            new ZonePartition<Zone.Water>());
+
+    public static final int REACH = 12;
+    public static final Board EMPTY = new Board(new PlacedTile[625], new int[625],
+            ZonePartitions.EMPTY);
+
+    private Board(PlacedTile[] placedTiles, int[] placedTileIndices, ZonePartitions boardPartitions) {
+        this.placedTiles =  placedTiles;
+        this.placedTileIndices = placedTileIndices;
+        this.boardPartitions = boardPartitions;
+    }
+}
