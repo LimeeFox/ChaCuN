3aff71e14d91a437f0ed6029558dd53003ce10e5
LimeeFox <limeefox@gmail.com>
2024-04-19 17:54:02 +0200

    finished ImageLoader

---
+++ src/ch/epfl/chacun/gui/ImageLoader.java
@@ -0,0 +1,36 @@
+package ch.epfl.chacun.gui;
+
+import javafx.scene.image.Image;
+
+/**
+ * Classe qui offre des méthodes permettantes de charger des images des tuiles
+ */
+public abstract class ImageLoader {
+
+    public static final int LARGE_TILE_PIXEL_SIZE = 512; //Taille des grandes tuiles
+    public static final int LARGE_TILE_FIT_SIZE = 256; //Taille d'affichage des grandes tuiles
+    public static final int NORMAL_TILE_PIXEL_SIZE = 256; //Taille des tuiles normales
+    public static final int NORMAL_TILE_FIT_SIZE = 128; //Taille d'affichage des tuiles normales
+    public static final int MARKER_PIXEL_SIZE = 96; //Taille du marqueur
+    public static final int MARKER_FIT_SIZE = 48; //Taille d'affichage du marqueur
+
+    /** @todo check si l'utilisation de cette methode gere elle-meme le cas ou tileId serait invalide
+     * Obtenir une tuile selon son @tileId de taille 256px
+     *
+     * @param tileId l'identifiant de la tuile
+     * @return une image de la tuile qui corréspond à @tileId
+     */
+    public static Image normalImageForTile(int tileId) {
+        return new Image("/256/tiles/" + tileId + ".jpg");
+    }
+
+    /** @todo check si l'utilisation de cette methode gere elle-meme le cas ou tileId serait invalide
+     * Obtenir une tuile selon son @tileId de taille 512px
+     *
+     * @param tileId l'identifiant de la tuile
+     * @return une image de la tuile qui corréspond à @tileId
+     */
+    public static Image largeImageForTile(int tileId) {
+        return new Image("/512/tiles/" + tileId + ".jpg");
+    }
+}
