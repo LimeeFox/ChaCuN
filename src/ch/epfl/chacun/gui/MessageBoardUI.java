8615046cf7c9b88afb431cb92fec30013230b963
Cyriacph <116813319+Cyriacph@users.noreply.github.com>
2024-04-26 15:30:34 +0200

    commented MessageBoardUI

--- src/ch/epfl/chacun/gui/MessageBoardUI.java
+++ src/ch/epfl/chacun/gui/MessageBoardUI.java
@@ -21,29 +21,44 @@ import java.util.Set;
 
 import static javafx.application.Platform.runLater;
 
+/**
+ * Gestion de l'interface graphique du tableau d'affichage de messages
+ */
 public class MessageBoardUI {
 
+    /**
+     * Création et mis à jour de l'affichage du tableau d'affichage de messages du jeu
+     *
+     * @param messages
+     *          messages à faire afficher sur notre tableau d'affichage
+     * @param tileIds
+     *          identifiant des tuiles concernées par les messages (mis à jour ultérieurement)
+     * @return un tableau d'affichage de messages défilable et mis à jour
+     */
     public static Node create(ObservableValue<List<MessageBoard.Message>> messages,
                               ObjectProperty<Set<Integer>> tileIds) {
 
+        // Initialisation de l'espace d'affichage de message
         VBox root = new VBox();
 
+        // Initialisation du tableau défilable
         ScrollPane scrollPane = new ScrollPane(root);
-        //todo add path as string
-        scrollPane.getStylesheets()
-                .add("message-board.css");
-        //todo add thing next to hashtag
+        scrollPane.getStylesheets().add("message-board.css");
         scrollPane.setId("message-board");
 
+        // Mise en place de l'auditeur pour notre tableau d'affichage
         messages.addListener((observable, oldMessages, newMessages) -> {
+            // Itération à travers les nouveaux messages
             for (MessageBoard.Message message : newMessages) {
+                // On vérifie que le nouveau message concerné n'est pas déjà présent dans les anciens messages
                 if (!oldMessages.contains(message)) {
+                    // Initialisation d'un nouveau text, à la taille spécifié (256)
                     Text text = new Text(message.text());
                     text.setWrappingWidth(ImageLoader.LARGE_TILE_FIT_SIZE);
-                    //todo add children
                     root.getChildren().add(text);
 
-                    // Set on mouse entered event
+                    // Lorsque la souris survole un message, on ajoute l'identifiant des tuiles concernées aux
+                    // identifiants des tuiles qu'on souhaite surligner (highlight)
                     text.setOnMouseEntered(event -> {
                         tileIds.set(message.tileIds());
                     });
