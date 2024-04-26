4ee7902e7272c5a4d44c6f3c1405a46a4ffc512e
Cyriacph <116813319+Cyriacph@users.noreply.github.com>
2024-04-26 15:21:04 +0200

    finished MessageBoardUI

--- src/ch/epfl/chacun/gui/MessageBoardUI.java
+++ src/ch/epfl/chacun/gui/MessageBoardUI.java
@@ -6,31 +6,56 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static javafx.application.Platform.runLater;
+
 public class MessageBoardUI {
 
-    public static void create(ObservableValue<List<MessageBoard.Message>> messages,
+    public static Node create(ObservableValue<List<MessageBoard.Message>> messages,
                               ObjectProperty<Set<Integer>> tileIds) {
-        ListView<MessageBoard.Message> messageListView = new ListView<>();
 
-        VBox root = new VBox(messageListView);
-        //todo modify size accordingly
-        Scene scene = new Scene(root, 400, 300);
+        VBox root = new VBox();
 
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.show();
+        ScrollPane scrollPane = new ScrollPane(root);
+        //todo add path as string
+        scrollPane.getStylesheets()
+                .add("message-board.css");
+        //todo add thing next to hashtag
+        scrollPane.setId("message-board");
 
-        messages.addListener((observable, oldMessages ,newMessages) -> {
-            ObservableList<MessageBoard.Message> observableMessages = FXCollections.observableArrayList(newMessages);
-            messageListView.setItems(observableMessages);
+        messages.addListener((observable, oldMessages, newMessages) -> {
+            for (MessageBoard.Message message : newMessages) {
+                if (!oldMessages.contains(message)) {
+                    Text text = new Text(message.text());
+                    text.setWrappingWidth(ImageLoader.LARGE_TILE_FIT_SIZE);
+                    //todo add children
+                    root.getChildren().add(text);
+
+                    // Set on mouse entered event
+                    text.setOnMouseEntered(event -> {
+                        tileIds.set(message.tileIds());
+                    });
+
+                    // Set on mouse exited event
+                    text.setOnMouseExited(event -> {
+                        tileIds.set(new HashSet<>());
+                    });
+                }
+            }
+            runLater(() -> scrollPane.setVvalue(1));
         });
+        return scrollPane;
     }
 }
