5aae709785273665f511bf10ab2c3b8f0faba1d3
LimeeFox <50243381+LimeeFox@users.noreply.github.com>
2024-04-26 10:48:29 +0200

    image loader (#45)
    * imported resource files
    
    * finished ImageLoader
    
    * started PlayersUI
    
    * added author
    
    ---------
    
    Co-authored-by: LimeeFox <limeefox@gmail.com>

---
+++ src/ch/epfl/chacun/gui/PlayersUI.java
@@ -0,0 +1,15 @@
+package ch.epfl.chacun.gui;
+
+import ch.epfl.chacun.GameState;
+import ch.epfl.chacun.TextMaker;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Node;
+
+/**
+ *
+ */
+public abstract class PlayersUI {
+    public static Node create(ObservableValue<GameState> currentGameState, TextMaker textMaker) {
+
+    }
+}
