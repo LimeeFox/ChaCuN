041b26bea960f35f4bde8d6a804795423c78ebe2
Cyriacph <116813319+Cyriacph@users.noreply.github.com>
2024-05-01 22:10:03 +0200

    added getCache

--- src/ch/epfl/chacun/gui/BoardUI.java
+++ src/ch/epfl/chacun/gui/BoardUI.java
@@ -3,9 +3,15 @@ package ch.epfl.chacun.gui;
 import ch.epfl.chacun.*;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 
@@ -15,6 +21,18 @@ import java.util.function.Consumer;
  * @author Cyriac Philippe (360553)
  */
 public class BoardUI {
+    /**
+     *
+     * @param scope
+     * @param currentGameState
+     * @param tileRotation
+     * @param visibleOccupants
+     * @param tiles
+     * @param tileRotates
+     * @param tileMoves
+     * @param occupantConsumer
+     * @return
+     */
     public static Node create(int scope, GameState currentGameState, Rotation tileRotation,
                               Set<Occupant> visibleOccupants, Set<Integer> tiles,
                               Consumer<Rotation> tileRotates, Consumer<Pos> tileMoves,
@@ -22,19 +40,57 @@ public class BoardUI {
         // La portée doit toujours être strictement positive
         Preconditions.checkArgument(scope > 0);
 
+        // Table associant les identifiants des tuiles du jeu à leur image
+        // Les images de chaque tuile sont ainsi toutes chargées
+        Map<Integer, Image> cache = getCache();
+
+        //todo might have to define this somewhere else, but i doubt it
+        WritableImage emptyTileImage = new WritableImage(1, 1);
+        emptyTileImage
+                .getPixelWriter()
+                .setColor(0, 0, Color.gray(0.98));
+
         GridPane boardGridPane = new GridPane();
         for (int x = -scope; x < scope; x++) {
             for (int y = -scope; y < scope; y++) {
-                //todo, two cases :
-                //todo 1, we do have a tile to add
-                //todo 2, the tile to add is null (what to do then?)
-                ImageView imageView = new ImageView(ImageLoader.normalImageForTile(currentGameState.board()
-                        .tileAt(new Pos(x, y)).tile().id()));
-                boardGridPane.add(imageView, x + scope, y+ scope);
+                Pos position = new Pos(x, y);
+                Board currentBoard = currentGameState.board();
+
+                PlacedTile placedTile = currentBoard.tileAt(position);
+
+                // Si la position x,y que l'on observe contient une tuile, on l'affiche
+                if (placedTile != null) {
+                    boardGridPane.add(new ImageView(cache.get(placedTile.id())),
+                            x + scope, y+ scope);
+                }
+                    // Sinon on affiche un case "vide" (presque blanche)
+                else {
+                        boardGridPane.add(new ImageView(emptyTileImage), x + scope, y + scope);
+                }
             }
         }
+        return boardGridPane;
+    }
 
-        //todo return the right type of node once it has been modified accordingly
-        return null;
+    /**
+     *
+     * @return
+     */
+    private static Map<Integer, Image> getCache() {
+        Map<Integer, Image> cache = new HashMap<>();
+        for (int i = 0; i < 95; i++) {
+            // Chemin vers l'image de tuile
+            //todo check if we need to get the images for board tile from 256 or 512
+            String imagePath = "C:\\Users\\cyria\\OneDrive\\Documents\\GITHUB\\POOP\\ChaCuN\\resources\\256\\" +
+                    STR."\{i}.jpg";
+
+            // Chargement de l'image
+            File imageFile = new File(imagePath);
+            Image image = new Image(imageFile.toURI().toString());
+
+            // Ajout de l'image à la "cache"
+            cache.put(i, image);
+        }
+        return cache;
     }
 }
