c490914428a0777cfd3310b8651f8f7e1beda247
Cyriacph <116813319+Cyriacph@users.noreply.github.com>
2024-05-03 19:07:55 +0200

    added 2 encodeBits methods

--- src/ch/epfl/chacun/Base32.java
+++ src/ch/epfl/chacun/Base32.java
@@ -14,5 +14,28 @@ public class Base32 {
         return s.chars().allMatch(c -> ALPHABET.indexOf((char) c) != -1);
     }
 
+    /**
+     * Encode en base 32 un entier avec une représentation binaire en 5 bits
+     *
+     * @param n
+     *      entier à encoder
+     * @return une chaîne de charactèrs de longueur 1 contenant la valeur en base 32 de l'entier donné
+     */
+    public String encodeBits5(int n) {
+        return String.valueOf(ALPHABET.charAt(n));
+    }
+
+    /**
+     * Encode en base 32 un entier avec une représentation binaire en 10 bits
+     *
+     * @param n
+     *      entier à encoder
+     * @return  uen chaîné de charactèrs de longueur 2 contenant la valeur en base 32 de l'entier donné
+     */
+    public String encodeBits10(int n) {
+        int n1 = n >>> 5;
+        int n2 = n & 0b1111;
+        return encodeBits5(n1) + encodeBits5(n2);
+    }
 
 }
