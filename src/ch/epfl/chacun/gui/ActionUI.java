9e2356bdfcfcc1c0fa8003ee485e0af1006d91c6
Cyriacph <116813319+Cyriacph@users.noreply.github.com>
2024-05-21 13:37:26 +0200

    documented ActionUI.create

--- src/ch/epfl/chacun/gui/ActionUI.java
+++ src/ch/epfl/chacun/gui/ActionUI.java
@@ -1,7 +1,6 @@
 package ch.epfl.chacun.gui;
 
 import ch.epfl.chacun.Base32;
-import javafx.beans.binding.Bindings;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
@@ -11,16 +10,21 @@ import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 import java.util.List;
-import java.util.StringJoiner;
 import java.util.function.Consumer;
 
 public class ActionUI {
     /**
-     * todo add javadoc pls
+     * Créer le graphe de scène qui gère les codes en base 32 pour le jeu à distance composé :
+     *      d'une instance de Text qui affiche les 4 derniers codes en base 32, dans l'ordre chronologique,
+     *      d'une instance de TextFiel pour entrer les codes en base 32 et les appliquer à la partie
      *
      * @param base32Codes
+     *          valeur observable de la liste de chaînes de charactèrs correspondants aux codes en base 32 de châque
+     *          action de la partie, dans l'ordre chronologique
      * @param handler
-     * @return
+     *          gestionnaire d'événement qui accepte la représentation en base 32 d'une action
+     * @return root
+     *          graphe de scène de l'interface de jeu à distance
      */
     public static Node create(ObservableValue<List<String>> base32Codes, Consumer<String> handler) {
         HBox root = new HBox();
@@ -32,7 +36,26 @@ public class ActionUI {
         Text lastActionsText = new Text();
         lastActionsText.textProperty().bind(base32Codes.map((ActionUI::formatBase32Codes)));
 
+        // Nouveau champ de text servant à ajouter, et faire appliquer des codes en base 32 à la partie
+        TextField actionField = getTextField(handler);
 
+        // Ajout des enfants au graph de scène
+        root.getChildren().add(lastActionsText);
+        root.getChildren().add(actionField);
+
+        return root;
+    }
+
+    /**
+     * Méthode d'aide qui permet de créer une instance de champ textuel qui n'accepte que les charactèrs de l'alphabet
+     * base 32 et transforme toutes les majuscules en minuscules
+     *
+     * @param handler
+     *          gestionnaire d'événement qui accepte la représentation en base 32 d'une action
+     * @return actionField
+     *          le champ textuel qui accepte des codes en base 32
+     */
+    private static TextField getTextField(Consumer<String> handler) {
         TextField actionField = new TextField();
         actionField.setTextFormatter(new TextFormatter<>(change -> {
             change.setText(change.getText().toUpperCase());
@@ -45,15 +68,11 @@ public class ActionUI {
                 actionField.clear();
             }
         });
-
-        root.getChildren().add(lastActionsText);
-        root.getChildren().add(actionField);
-
-        return root;
+        return actionField;
     }
 
     /**
-     * Méthode d'aide qui permet d'afficher les 4 derniers codes en base32 d'une liste donnée selon le format:
+     * Méthode d'aide qui permet d'afficher les 4 derniers codes en base32 d'une liste donnée selon le format :
      *          "numéro d'apparition du code : code en base32"
      *
      * @param codes
